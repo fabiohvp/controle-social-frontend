6f05ba0fd5eff94887779ed81e4c9ab45990eb8b
Fabio Henriques <fabiohvp@hotmail.com>
2023-07-10 20:15:36 -0300

    add sidebar hover events and animation

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { useAtom } from "jotai";
-import { CSSProperties, ReactNode } from "react";
+import { CSSProperties, ReactNode, useEffect, useState } from "react";
 import { SidebarList } from "./SidebarHelper";
 import { sidebarOpen } from "./sidebar-state";
+
+const ANIMATION_SPEED = "0.25s";
+const CLASS_CLOSED_WIDTH = "w-[50px]";
+const CLASS_OPENED_WIDTH = "w-[230px]";
 
 type Props = {
   children: ReactNode;
@@ -11,16 +15,40 @@ type Props = {
 };
 
 export default function Sidebar(props: Props) {
-  const [open, setOpen] = useAtom(sidebarOpen);
+  const [open] = useAtom(sidebarOpen);
+  const [width, setWidth] = useState(getWidth(open));
+
+  useEffect(() => {
+    setWidth(getWidth(open));
+  }, [open]);
+
+  function onMouseEnter(_: React.MouseEvent<HTMLUListElement>) {
+    if (open) return;
+    setWidth(CLASS_OPENED_WIDTH);
+  }
+
+  function onMouseLeave(_: React.MouseEvent<HTMLUListElement>) {
+    if (open) return;
+    setWidth(CLASS_CLOSED_WIDTH);
+  }
 
   return (
     <SidebarList
-      className={`bg-blue-menu h-screen overflow-y-auto py-1 text-white ${
-        open ? "w-[230px]" : "w-[50px]"
-      } ${props.className ?? ""}`}
-      style={props.style}
+      className={`bg-blue-menu h-screen overflow-y-auto py-1 text-white ${width} ${
+        props.className ?? ""
+      }`}
+      style={{
+        ...props.style,
+        transition: `width ${ANIMATION_SPEED} ease-${open ? "in" : "out"}`,
+      }}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       {props.children}
     </SidebarList>
   );
+}
+
+function getWidth(open: boolean) {
+  return open ? CLASS_OPENED_WIDTH : CLASS_CLOSED_WIDTH;
 }
--- src/components/layout/sidebar/SidebarHelper.tsx
+++ src/components/layout/sidebar/SidebarHelper.tsx
@@ -4,11 +4,18 @@ type Props = {
   children: ReactNode;
   className?: string;
   style?: CSSProperties;
+  onMouseEnter?: (event: React.MouseEvent<HTMLUListElement>) => void;
+  onMouseLeave?: (event: React.MouseEvent<HTMLUListElement>) => void;
 };
 
 export function SidebarList(props: Props) {
   return (
-    <ul className={`${props.className ?? ""}`} style={props.style}>
+    <ul
+      className={`${props.className ?? ""}`}
+      style={props.style}
+      onMouseEnter={props.onMouseEnter}
+      onMouseLeave={props.onMouseLeave}
+    >
       {props.children}
     </ul>
   );
