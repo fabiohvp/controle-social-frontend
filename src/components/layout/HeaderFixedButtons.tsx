b6b6387235f414a2b622eb93799b9dbdcb18c577
Fabio Henriques <fabiohvp@hotmail.com>
2023-07-17 15:59:09 -0300

    fix logo image path

--- src/components/layout/HeaderFixedButtons.tsx
+++ src/components/layout/HeaderFixedButtons.tsx
@@ -1,7 +1,7 @@
+import TCEESLogo from "@/components/images/logos/TCEESLogo";
 import Link from "next/link";
 import { FaLock } from "react-icons/fa6";
 import { twMerge } from "tailwind-merge";
-import TCEESLogo from "../images/logos/TCEESLogo";
 
 type Props = {
   className?: string;
