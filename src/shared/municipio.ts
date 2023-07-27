636ebb6eb487dd9cca242a059e7f601410e981f8
Fabio Henriques <fabiohvp@hotmail.com>
2023-07-27 01:21:58 -0300

    fix getMunicipios()

--- src/shared/municipio.ts
+++ src/shared/municipio.ts
@@ -1,19 +1,66 @@
-import MUNICIPIOS from "@/data/municipios.json";
+import { normalize } from "@/formatters/string";
+import { cache } from "react";
 
-export type Municipio = (typeof MUNICIPIOS)[0];
+export type Municipio = {
+  codigo: string;
+  nome: string;
+  nomeNormalizado: string;
+};
+
+//export type Municipio = (typeof MUNICIPIOS)[0];
 export type MunicipiosProps = {
   municipios: Municipio[];
 };
 
-export async function getMunicipios(): Promise<Municipio[]> {
-  console.log(
-    `MUNICIPIO ---- ${process.env.VERCEL_PROTOCOL}://${process.env.VERCEL_URL}/api/municipio`
-  );
-  const res = await fetch(
-    `${process.env.VERCEL_PROTOCOL}://${process.env.VERCEL_URL}/api/municipio`
-  );
-  return res.json();
-}
+type EsferaAdministrativa = {
+  codigoEsferaAdministrativa: string;
+  nomeEsferaAdministrativa: string;
+};
+
+export const getMunicipios = cache(async (): Promise<Municipio[]> => {
+  if (process.env.npm_lifecycle_event === "build") {
+    const res = await fetch(
+      "https://paineldecontrole.tcees.tc.br/api/Settings/GetAll?v=26-07-2023-1690391164330"
+    );
+    return res
+      .json()
+      .then(
+        ({
+          esferasAdministrativas,
+        }: {
+          esferasAdministrativas: EsferaAdministrativa[];
+        }) => {
+          const municipios: Municipio[] = [];
+
+          for (const esferaAdministrativa of Object.values(
+            esferasAdministrativas
+          )) {
+            municipios.push({
+              codigo: esferaAdministrativa.codigoEsferaAdministrativa,
+              nome: esferaAdministrativa.nomeEsferaAdministrativa,
+              nomeNormalizado: normalize(
+                esferaAdministrativa.nomeEsferaAdministrativa
+              ),
+            });
+          }
+          return municipios;
+        }
+      );
+  } else {
+    const res = await fetch(
+      `${process.env.VERCEL_PROTOCOL}://${process.env.VERCEL_URL}/api/municipio`
+    );
+    return res
+      .json()
+      .then((data) =>
+        data.map((d: Municipio) => ({
+          codigo: d.codigo,
+          nome: d.nome,
+          nomeNormalizado: d.nomeNormalizado,
+        }))
+      );
+  }
+});
 
 export function getCodigoMunicipio(
   municipios: Municipio[],
