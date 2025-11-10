a29d44b4628290e7825bb172a26845ca7923e4b2
Fabio Henriques <fabiohvp@hotmail.com>
2025-11-10 10:31:28 -0300

    remove webpack config since it is using turbo

--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ const nextConfig = {
 	experimental: {
     esmExternals: 'loose',
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.json$/,
-      type: 'json',
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
