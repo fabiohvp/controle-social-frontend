36e484b2ebd8189f5b6901430e5084232f0478c6
Fabio Henriques <fabiohvp@hotmail.com>
2025-11-10 01:06:20 -0300

    fix, debug

--- src/app/api/municipio/route.ts
+++ src/app/api/municipio/route.ts
@@ -2,5 +2,6 @@ import MUNICIPIOS from "@/data/municipios.json";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_: NextRequest) {
+	console.log('logg', MUNICIPIOS);
 	return NextResponse.json(MUNICIPIOS);
 }
