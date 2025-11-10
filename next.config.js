4e0cbd8fa9ba751f9256cc350cb2b7678f40db7e
Fabio Henriques <fabiohvp@hotmail.com>
2025-11-10 10:29:04 -0300

    change next config to try to import json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
+	experimental: {
+    esmExternals: 'loose',
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.json$/,
+      type: 'json',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- src/app/api/municipio/route.ts
+++ src/app/api/municipio/route.ts
@@ -2,6 +2,5 @@ import MUNICIPIOS from "@/data/municipios.json";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_: NextRequest) {
-	console.log('logg', MUNICIPIOS);
 	return NextResponse.json(MUNICIPIOS);
 }
