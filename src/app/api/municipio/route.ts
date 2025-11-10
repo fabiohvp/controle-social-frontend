385cca474e69c329e772f5087549c04eb9ea9b0e
Fabio Henriques <fabiohvp@hotmail.com>
2025-11-09 22:11:17 -0300

    add log to check json error

--- src/app/api/municipio/route.ts
+++ src/app/api/municipio/route.ts
@@ -2,5 +2,6 @@ import MUNICIPIOS from "@/data/municipios.json";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_: NextRequest) {
+	console.log(MUNICIPIOS);
   return NextResponse.json(MUNICIPIOS);
 }
